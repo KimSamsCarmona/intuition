222e85954c81d088a293a93049aa4e035540af35
Kim Carmona <kimsams@hotmail.com>
2022-05-06 12:26:22 -0500

    button styling

--- src/Components/GameComponent.js
+++ src/Components/GameComponent.js
@@ -1,30 +1,6 @@
 import * as React from 'react';
 import { Box, Card, CardContent, CardActions, Button, Typography } from '@mui/material';
 
-
-function Item(props) {
-  const { sx, ...other } = props;
-  return (
-    <Box
-      sx={{
-        p: 1,
-        m: 1,
-        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#101010' : 'grey.100'),
-        color: (theme) => (theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800'),
-        border: '1px solid',
-        borderColor: (theme) =>
-          theme.palette.mode === 'dark' ? 'grey.800' : 'grey.300',
-        borderRadius: 2,
-        fontSize: '0.875rem',
-        fontWeight: '700',
-        ...sx,
-      }}
-      {...other}
-    />
-  );
-}
-
-
 export const GameComponent = () => {
   return (
     <div>
@@ -42,10 +18,21 @@ export const GameComponent = () => {
         bgcolor: '#9BD8E659',
         borderRadius: 1,
       }}>
-      <Button sx={{ bgcolor: '#FFFFFFBF', p:2, m:2 }}>Item 1</Button>
-      <Button sx={{ bgcolor: '#FFFFFFBF', p:2, m:2 }}>Item 2</Button>
-      <Button sx={{ bgcolor: '#FFFFFFBF', p:2, m:2 }}>Item 3</Button>
+      <Button variant='contained' sx={{ p:2, m:2, bgcolor:'#FFFFFFBF'}}>Item 1</Button>
+      <Button variant='contained' sx={{ p:2, m:2, bgcolor:'#FFFFFFBF'}}>Item 1</Button>
+      <Button variant='contained' sx={{ p:2, m:2, bgcolor:'#FFFFFFBF'}}>Item 1</Button>
     </Card>
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        p: 4
+      }}>
+      <Button fullwidth size='large' variant='contained' style={{ width: '100%', height: '63px', backgroundColor: '#2596BE', color: 'black', fontSize: '2rem', fontWeight: 'bolder' }}>
+          Submit
+      </Button>
+    </Box>
   </div>
   );
 }
