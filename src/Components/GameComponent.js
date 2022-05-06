d70e4429799c7f5102461402f74cf1208cb4b664
Kim Carmona <kimsams@hotmail.com>
2022-05-06 16:34:07 -0500

    onClick inital setup

--- src/Components/GameComponent.js
+++ src/Components/GameComponent.js
@@ -3,44 +3,56 @@ import { Box, Card, Button, Typography, styled } from '@mui/material';
 
 import { emojis } from '../shared/emojis';
 
-export const GameComponent = () => {
+export const GameComponent = (props) => {
   //randomly pick emoji
-  const emoji1 = emojis[Math.floor(Math.random()*emojis.length)];
-  const emoji2 = emojis[Math.floor(Math.random()*emojis.length)];
-  const emoji3 = emojis[Math.floor(Math.random()*emojis.length)];
+  const emoji1 = emojis[Math.floor(Math.random() * emojis.length)];
+  const emoji2 = emojis[Math.floor(Math.random() * emojis.length)];
+  const emoji3 = emojis[Math.floor(Math.random() * emojis.length)];
 
   //show selected
-  const EmojiButton = styled(Button) ({
+  const EmojiButton = styled(Button)({
     '&:hover': {
-      backgroundColor:'#9bd8e6'
+      backgroundColor: '#9bd8e6'
     },
     '&:focus': {
-      boxShadow: '0 0 0 0.2rem rgba(155, 216, 230, .5)', 
+      boxShadow: '0 0 0 0.2rem rgba(155, 216, 230, .5)',
       backgroundColor: '#2596BE'
     },
   });
 
+  const emojiClick1=() => {
+    alert(emoji1);
+  }
+
+  const emojiClick2=() => {
+    alert(emoji2);
+  }
+
+  const emojiClick3=() => {
+    alert(emoji3)
+  }
+
   return (
     <div>
       <Typography variant='h2' gutterBottom style={{ color: '#245B67', fontWeight: '900' }}>
-      Pick one
-    </Typography>
-    <Card
-      style={{ width: '90%' }}
-      sx={{
-        display: 'grid',
-        gridTemplateRows: 'repeat(3, 1fr)',
-        alignContent: 'center',
-        p: 4,
-        mx: 'auto',
-        bgcolor: '#9BD8E659',
-        borderRadius: 1,
-      }}>
-      <EmojiButton variant='contained' sx={{ p:2, m:2, bgcolor:'#FFFFFFFF', fontSize: 50 }}>{emoji1}</EmojiButton>
-      <EmojiButton variant='contained' sx={{ p:2, m:2, bgcolor:'#FFFFFFFF', fontSize: 50 }}>{emoji2}</EmojiButton>
-      <EmojiButton variant='contained' sx={{ p:2, m:2, bgcolor:'#FFFFFFFF', fontSize: 50 }}>{emoji3}</EmojiButton>
-    </Card>
-    {/* <Box
+        Pick one
+      </Typography>
+      <Card
+        style={{ width: '90%' }}
+        sx={{
+          display: 'grid',
+          gridTemplateRows: 'repeat(3, 1fr)',
+          alignContent: 'center',
+          p: 4,
+          mx: 'auto',
+          bgcolor: '#9BD8E659',
+          borderRadius: 1,
+        }}>
+        <EmojiButton variant='contained' sx={{ p: 2, m: 2, bgcolor: '#FFFFFFFF', fontSize: 50 }} onClick={emojiClick1}>{emoji1}</EmojiButton>
+        <EmojiButton variant='contained' sx={{ p: 2, m: 2, bgcolor: '#FFFFFFFF', fontSize: 50 }} onClick={emojiClick2}>{emoji2}</EmojiButton>
+        <EmojiButton variant='contained' sx={{ p: 2, m: 2, bgcolor: '#FFFFFFFF', fontSize: 50 }} onClick={emojiClick3}>{emoji3}</EmojiButton>
+      </Card>
+      {/* <Box
       sx={{
         display: 'flex',
         alignItems: 'center',
@@ -63,6 +75,6 @@ export const GameComponent = () => {
           Submit
       </Button>
     </Box> */}
-  </div>
+    </div>
   );
 }
