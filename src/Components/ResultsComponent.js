e3f173589ed1176d5d5439374c7b1c2aa7dd7d3b
Kim Carmona <kimsams@hotmail.com>
2022-05-06 17:19:57 -0500

    grid layout for results component

--- src/Components/ResultsComponent.js
+++ src/Components/ResultsComponent.js
@@ -1,9 +1,44 @@
 import * as React from 'react';
+import { Avatar, Button, Card, Grid, Typography } from '@mui/material';
 
 export const ResultsComponent = () => {
+
   return (
     <div>
-      Hello Results Page
+      <Typography variant='h2' gutterBottom style={{ color: '#245B67', fontWeight: '900' }}>
+        You Matched!
+      </Typography>
+      <Card
+        style={{ width: '90%' }}
+        sx={{
+          display: 'grid',
+          alignContent: 'center',
+          p: 4,
+          mx: 'auto',
+          bgcolor: '#9BD8E659',
+          borderRadius: 1,
+        }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography variant='h4'>Player 1</Typography>
+          </Grid>
+          <Grid item xs={4}>
+            <Avatar sx={{ width: 60, height: 60 }}>KS</Avatar>
+          </Grid>
+          <Grid item xs={8}>
+            <Typography> Results Window</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant='h4'>Player 2</Typography>
+          </Grid>
+          <Grid item xs={4}>
+            <Avatar sx={{ width: 60, height: 60 }}>FC</Avatar>
+          </Grid>
+          <Grid item xs={8}>
+            <Typography> Results Window</Typography>
+          </Grid>
+        </Grid>
+      </Card>
     </div>
   );
 }
