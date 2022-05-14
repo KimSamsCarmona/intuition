de446f1c994160c9f6c019acc9d6b01038c1817a
Kim Carmona <kimsams@hotmail.com>
2022-05-14 18:47:53 -0500

    setting up socket.io server

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname, '/../public');
+const port = process.env.PORT || 3000;
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
+app.use(express.static(public path));
+server.listen(port, () => {
+  console.log(`Server is up on port ${port}.`)
+});
