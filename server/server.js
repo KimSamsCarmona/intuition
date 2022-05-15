c4396f818f836bdddc307b950547e2c12a41ebe3
Kim Carmona <kimsams@hotmail.com>
2022-05-14 19:19:54 -0500

    more server work (still not working)

--- server/server.js
+++ server/server.js
@@ -1,15 +1,35 @@
-const path = require('path');
-const http = require('http');
-const express = require('express');
-const socketIO = require('socket.io');
+// const path = require('path');
+const http = require("http");
+const express = require("express");
+const socketIo = require("socket.io");
 
-const publicPath = path.join(__dirname, '/../public');
+// const publicPath = path.join(__dirname, '/../public');
 const port = process.env.PORT || 3000;
-let app = express();
-let server = http.createServer(app);
-let io = socketIO(server);
+const index = require("./routes/index");
 
-app.use(express.static(public path));
-server.listen(port, () => {
-  console.log(`Server is up on port ${port}.`)
+const app = express();
+app.use(index);
+
+const server = http.createServer(app);
+const io = socketIo(server);
+
+let interval;
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+  });
 });
+
+const getApiAndEmit = socket => {
+  const response = new Date();
+  socket.emit("FromAPI", response);
+}
+
+server.listen(port, () => console.log(`Listening on port ${port}`))
